af4c8abc078fe841540974432ee38b52c63e6604
Ethem <koraygokdeniz@gmail.com>
2022-11-11 04:54:52 -0500

    new ip

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "44.201.219.2059";
+                        String gridAddress = "3.84.211.161/";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
